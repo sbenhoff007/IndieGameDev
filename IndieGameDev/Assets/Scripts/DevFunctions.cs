71fda78219baeb065f8638645109fba72dba2cbc
Shelley Benhoff <shelley@hoffstech.com>
2022-08-25 00:01:46 -0400

    Added controls dialog

--- IndieGameDev/Assets/Scripts/DevFunctions.cs
+++ IndieGameDev/Assets/Scripts/DevFunctions.cs
@@ -5,9 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class DevFunctions : MonoBehaviour
 {
+    GamePlaySystem gamePlaySystem;
+
     public void ResetPlayerPrefs()
     {
         PlayerPrefs.DeleteAll();
         SceneManager.LoadScene("SampleScene");
+    }
+
+    public void ShowControls()
+    {
+        gamePlaySystem = GetComponent<GamePlaySystem>();
+        string infoText = "F: Fishing | C: Catching | X: Interacting \r\n Arrows: Moving";
+        gamePlaySystem.ShowInfoDialog(infoText, 3f);
     }
 }
